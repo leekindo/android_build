cdf0bca24c50f584889d6315998b6166a831b31d
leekindo <leekindo@gmail.com>
2017-03-30 18:11:10 +0800

    revert CLANG LLVM Version

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,8 +1,8 @@
 ## Clang/LLVM release versions.
 LLVM_RELEASE_VERSION := 5.0
-LLVM_PREBUILTS_VERSION := 5.0
+LLVM_PREBUILTS_VERSION := clang-2690385
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
 
 # RenderScript-specific tools
-RS_LLVM_PREBUILTS_VERSION := 5.0
+RS_LLVM_PREBUILTS_VERSION := clang-2690385
 RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
